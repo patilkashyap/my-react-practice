8058dc61318c8727d1ffb0b2dae843b21ccbc43b
Kashyap Patil <kashyapdpatil@gmail.com>
2026-07-15 14:49:39 +0530

    Change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Info from './Info'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Info from "./Info";
 
-import { Suspense,lazy } from 'react'
+import { Suspense, lazy } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-    <Info />
+      <Info />
     </>
-    
-    )
-    
+  );
 }
-function  Hello({age="19",place}) {
-      return (
-        <p>My age is {age} and i live in {place}.</p>
-      )
-      }
-
-export default App
+export default App;
